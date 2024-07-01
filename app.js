19a9d57486c8271364b860dc00076cc3c91abb31
Shreedhar Halli <mayurbusinesssolutions@gmail.com>
2024-07-01 10:20:31 +0530

    changed webVersion to 2.2413.51-beta

--- app.js
+++ app.js
@@ -179,10 +179,10 @@ function whatsappFactoryFunction(clientId) {
     authStrategy: new LocalAuth({
       clientId: clientId,
     }),
-    webVersion: "2.2412.54",
+    webVersion: '2.2413.51-beta',
     webVersionCache: {
        type: 'remote',
-       remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html",
+       remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html'
        //remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",// old
    }
   });
