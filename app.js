0692d2ae9ce0314c69eb492da57dc5dec55c96c1
Shreedhar Halli <mayurbusinesssolutions@gmail.com>
2024-07-01 10:37:25 +0530

    removed webversion config

--- app.js
+++ app.js
@@ -179,12 +179,10 @@ function whatsappFactoryFunction(clientId) {
     authStrategy: new LocalAuth({
       clientId: clientId,
     }),
-    webVersion: '2.2413.51-beta',
+    // webVersion: '2.2413.51-beta',
     webVersionCache: {
-       type: 'remote',
-       remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html'
-       //remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",// old
-   }
+      type: 'none'
+  }
   });
   return client;  // Return the client instance, not the Client class
 }
